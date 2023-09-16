42ea7e34ead11f31e044dbb71c366f481fef6cef
ifsawal <102026588+ifsawal@users.noreply.github.com>
2023-09-16 23:16:07 +0700

    rubah dikit

--- app/Http/Controllers/Api/Pelanggan/MobTagihanController.php
+++ app/Http/Controllers/Api/Pelanggan/MobTagihanController.php
@@ -114,8 +114,8 @@ class MobTagihanController extends Controller
             }
 
 
-            // DB::commit();
-            DB::rollback();
+            DB::commit();
+            // DB::rollback();
             return response()->json([
                 "sukses" => true,
                 "pesan" => "Sukses membuat tagihan...",
