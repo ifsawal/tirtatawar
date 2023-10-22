138470e7cd5383e6df52d2aa22f72f6c719dfc54
ifsawal <ifsawal@gmail.com>
2023-10-22 18:16:36 +0700

    memperbaiki laporan bulanan

--- app/Http/Controllers/Api/Laporan/LaporanBulananController.php
+++ app/Http/Controllers/Api/Laporan/LaporanBulananController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Laporan;
 
 use App\Http\Controllers\Controller;
+use App\Models\Master\Pencatatan;
 use Illuminate\Http\Request;
 
 class LaporanBulananController extends Controller
@@ -20,7 +21,49 @@ class LaporanBulananController extends Controller
         $this->validate($r, [
             'bulan' => 'required',
             'tahun' => 'required',
+
         ]);
+
+        $catat=Pencatatan::query();
+        $catat->select(
+         'tagihans.id'
+        ,'pencatatans.bulan'
+        ,'pencatatans.tahun'
+        ,'tagihans.total'
+        ,'tagihans.status_bayar'
+        ,'tagihans.sistem_bayar'
+        ,'tagihans.tgl_bayar'
+
+    );
+        $catat->join('tagihans', 'tagihans.pencatatan_id', '=', 'pencatatans.id');
+        $catat->join('pelanggans', 'pelanggans.id', '=', 'pencatatans.pelanggan_id');
+        $catat->where('pencatatans.bulan','=', $r->bulan);
+        $catat->where('pencatatans.tahun','=', $r->tahun);
+
+        isset($r->golongan_id)?$catat->where('pelanggans.golongan_id','=', $r->golongan_id):'';
+        isset($r->wiljalan_id)?$catat->where('pelanggans.wiljalan_id','=', $r->wiljalan_id):'';
+        isset($r->status_bayar)?$catat->where('tagihans.status_bayar','=', $r->status_bayar):'';
+
+
+        $catat->get();
+        
+        $catatan=$catat->paginate(50);
+        if(count($catatan)==0){
+
+            return response()->json([
+                "sukses" => false,
+                "pesan" => "Data tidak ditemukan...",
+            ], 404);            
+
+        }
+        
+
+        return response()->json([
+            "sukses" => true,
+            "pesan" => "Sukses, data ditemukan...",
+            "data"=>$catatan,
+        ], 202);
+
     }
 
 
