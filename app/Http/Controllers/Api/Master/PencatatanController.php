f777f1380550c9b784e3c88110316d3c17444882
ifsawal <ifsawal@gmail.com>
2024-08-31 19:08:37 +0700

    perubahan

--- app/Http/Controllers/Api/Master/PencatatanController.php
+++ app/Http/Controllers/Api/Master/PencatatanController.php
@@ -257,8 +257,8 @@ class PencatatanController extends Controller
             $input == "2023-11") and ($user_id == 1 or $user_id == 26)) {
         } else //HAPUS NANTIK 2 baris ini
 
-            // if ($input == "2024-07" && $edit==true) {
-            // } else //HAPUS NANTIK 2 baris ini
+            if ($input == "2024-08" && $edit==true) {
+            } else //HAPUS NANTIK 2 baris ini
 
             if ($input < Carbon::now()->format('Y-m')) {  //FITUR METERAN SEBELUMNYA TIDAK BOLEH DIISI
                 return response()->json([
