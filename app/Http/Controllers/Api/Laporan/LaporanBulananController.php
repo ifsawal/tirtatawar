b675b71fcee7d24fba5e87a3bf36c6256145a403
ifsawal <ifsawal@gmail.com>
2025-01-10 11:49:14 +0700

    u

--- app/Http/Controllers/Api/Laporan/LaporanBulananController.php
+++ app/Http/Controllers/Api/Laporan/LaporanBulananController.php
@@ -34,7 +34,7 @@ class LaporanBulananController extends Controller
                 'tagihans.jumlah',
                 'tagihans.biaya',
                 'tagihans.pajak',
-                'tagihans.total',
+                if('tagihans.status_bayar'='Y','tagihans.total','tagihans.total_nodenda') as 'total_tagihan',
                 'tagihans.status_bayar',
                 'tagihans.sistem_bayar',
                 'tagihans.tgl_bayar',
