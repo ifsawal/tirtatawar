8c517ea3b7b7bf99bf9b4d071322096c2ce2b1da
ifsawal <ifsawal@gmail.com>
2025-02-10 19:04:08 +0700

    u

--- app/Http/Controllers/Api/Laporan/LaporanBelumBayarController.php
+++ app/Http/Controllers/Api/Laporan/LaporanBelumBayarController.php
@@ -25,39 +25,76 @@ class LaporanBelumBayarController extends Controller
 
     public static function laporan_belum_bayar($tahun)
     {
+        // return Pelanggan::all()->count();
+        // $pel = Pelanggan::with([
+        //     'pencatatan' => function ($query) use ($tahun) {
+        //         $query->where('tahun', '=', $tahun);
+        //     },
+        //     'golongan:id,golongan',
+        //     'wiljalan:id,jalan',
+        //     'pencatatan.tagihan',
+        //     // 'pencatatan.tagihan' => function ($query) {
+        //     //     $query->where('status_bayar', '=', 'N');
+        //     // },
+        //     'pencatatan.tagihan.penagih:id,user_id,tagihan_id,jumlah',
+        //     'pencatatan.tagihan.penagih.user:id,nama'
+        // ],)
+        //     // ->where('id', 506)
+        //     // ->whereRelation('pencatatan', 'tahun', '=', $tahun)
+        //     // ->whereRelation('pencatatan.tagihan', 'status_bayar', '=', 'N')
+        //     // ->limit(10)
+        //     ->get();
 
         $pel = Pelanggan::with([
-            'pencatatan' => function ($query) use ($tahun) {
-                $query->where('tahun', '=', $tahun);
-            },
             'golongan:id,golongan',
             'wiljalan:id,jalan',
-            'pencatatan.tagihan',
-            'pencatatan.tagihan' => function ($query) {
-                $query->where('status_bayar', '=', 'N');
-            },
-            'pencatatan.tagihan.penagih:id,user_id,tagihan_id,jumlah',
-            'pencatatan.tagihan.penagih.user:id,nama'
-        ],)
-            // ->where('id', 1)
-            ->whereRelation('pencatatan', 'tahun', '=', $tahun)
-            ->whereRelation('pencatatan.tagihan', 'status_bayar', '=', 'N')
-            // ->limit(15)
+        ])
+
             ->get();
 
 
-
-
-
-
-
         $has = [];
-        $kolom_awal=4;   
+        $kolom_awal = 4;
 
-        $no=0;
+        $no = 0;
         foreach ($pel as $p) {
             $no++;
 
+
+
+
+            $q = Pencatatan::query();
+            $q->select(
+                'pencatatans.bulan',
+                'pencatatans.tahun',
+                'tagihans.total_nodenda',
+                'tagihans.status_bayar',
+            );
+            $q->join('tagihans', 'pencatatans.id', '=', 'tagihans.pencatatan_id');
+            $q->where('pencatatans.tahun', '=', $tahun);
+            $q->where('pencatatans.pelanggan_id', '=', $p->id);
+            $hasil = $q->get();
+
+            $b = array();
+            for ($a = 1; $a <= 12; $a++) {
+
+                $ditemukan = 0;
+                foreach ($hasil as $h) {
+                    if ($a == $h->bulan) {
+                        if ($h->status_bayar == "N") {
+                            $b[] = $h->total_nodenda;
+                        } else {
+                            $b[] = null;
+                        }
+                        $ditemukan = 1;
+                    } else {
+                        continue;
+                    }
+                }
+                if ($ditemukan == 0) {
+                    $b[] = null;
+                }
+            }
             $a = [
                 'no' => $no,
                 'nama' => $p->nama,
@@ -65,23 +102,10 @@ class LaporanBelumBayarController extends Controller
                 'wiljalan' => $p->wiljalan->jalan,
             ];
 
-            $b=[];
-            for ($i = 0; $i < 12; $i++) {
-                if (isset($p->pencatatan[$i]->tagihan->total_nodenda)) {
-                    $b[$i] = $p->pencatatan[$i]->tagihan->total_nodenda;
-                } else {
-                    $b[$i] = null;
-                }
-            }
-                     
             $c['total'] = "=SUM(E$kolom_awal:P$kolom_awal)";
-            $kolom_awal+=1;
+            $kolom_awal += 1;
 
-            $has[] = $a + $b+$c;
-
-
-
-
+            $has[] = $a + $b + $c;
         }
         return collect($has);
     }
