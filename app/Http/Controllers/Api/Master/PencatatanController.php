49b6d838b38321d84802871301fdd366500082d5
ifsawal <ifsawal@gmail.com>
2025-03-04 22:30:30 +0700

    u

--- app/Http/Controllers/Api/Master/PencatatanController.php
+++ app/Http/Controllers/Api/Master/PencatatanController.php
@@ -262,8 +262,8 @@ class PencatatanController extends Controller
             $input == "2023-11") and ($user_id == 1 or $user_id == 26)) {
         } else //HAPUS NANTIK 2 baris ini
 
-            if ($input == "2025-02" && $edit==true) {  //buka input semua orang
-            } else //HAPUS NANTIK 2 baris ini
+            // if ($input == "2025-02" && $edit==true) {  //buka input semua orang
+            // } else //HAPUS NANTIK 2 baris ini
 
             // if ($input == "2024-12" && $edit==true && $user_id==14) {   //buka input 1 orang
             // } else //HAPUS NANTIK 2 baris ini
