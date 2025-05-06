c8f16412698bccf3127abb6cb61bb0a2a8addf46
ifsawal <ifsawal@gmail.com>
2025-05-06 09:14:10 +0700

    u

--- app/Http/Controllers/Api/Master/PencatatanController.php
+++ app/Http/Controllers/Api/Master/PencatatanController.php
@@ -266,12 +266,13 @@ class PencatatanController extends Controller
             $input == "2025-02" or
             $input == "2025-03" or
             $input == "2025-03" or
+            $input == "2025-04" or
             $input == "2023-12" or
             $input == "2023-11") and ($user_id == 1 or $user_id == 26)) {
         } else //HAPUS NANTIK 2 baris ini
 
-            if ($input == "2025-04" && $edit==true) {  //buka input semua orang
-             } else //HAPUS NANTIK 2 baris ini
+            // if ($input == "2025-04" && $edit==true) {  //buka input semua orang
+            //  } else //HAPUS NANTIK 2 baris ini
 
             // if ($input == "2025-03" && $edit==true && $user_id==24) {   //buka input 1 orang 20
             // } else //HAPUS NANTIK 2 baris ini
