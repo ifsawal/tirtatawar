5ccd1829b5ffbb9422a5696c8e3e0417b79a5ab3
ifsawal <ifsawal@gmail.com>
2025-06-04 11:25:14 +0700

    u

--- app/Http/Controllers/Api/Master/PencatatanController.php
+++ app/Http/Controllers/Api/Master/PencatatanController.php
@@ -228,7 +228,7 @@ class PencatatanController extends Controller
         $tambahbulan = $sekarang->addMonth()->format('Y-m'); //tambah 1 bulan ke depan dari sekarang
 
 
-        if (date('Y-m') == $input && date('d') <= 2) {
+        if (date('Y-m') == $input && date('d') <= 20) {
             return response()->json([
                 "sukses" => false,
                 "pesan" => "Pencatatan meteraan bulan " . $input . " belum dibuka...",
