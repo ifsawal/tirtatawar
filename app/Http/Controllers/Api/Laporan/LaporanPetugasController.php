ec87230fccec2bf63619ca8ae47a0d9d6f5f8257
ifsawal <ifsawal@gmail.com>
2025-10-12 16:05:39 +0700

    u update

--- app/Http/Controllers/Api/Laporan/LaporanPetugasController.php
+++ app/Http/Controllers/Api/Laporan/LaporanPetugasController.php
@@ -288,7 +288,7 @@ class LaporanPetugasController extends Controller
             $lap->terbayar_no_denda =  $data['terbayar_no_denda'];//
             $lap->denda =  $data['denda'];//
             $lap->sisa =  $data['drd']-$data['terbayar_no_denda'];
-            $lap->persentase =  floor(($data['terbayar']/$data['drd'])*100);
+            $lap->persentase =  floor(($data['jumlah_terbayar']/$data['drd'])*100);
             $lap->total_rp =  $data['jumlah_rupiah'];
             $lap->rp_terbayar =  $data['jumlah_terbayar'];
             $lap->rp_no_bayar =  $data['jumlah_rupiah'] - $data['jumlah_terbayar'];
@@ -311,7 +311,7 @@ class LaporanPetugasController extends Controller
             "drd" => $drd,
             "terbayar" => $terbayar,
             "sisa" => $sisa,
-            "persentase" => floor(($terbayar/$drd)*100),
+            "total_persentase" => floor(($terbayar/$drd)*100),
 
         ], 201);
     }
